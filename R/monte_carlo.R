f6e50ad277c2256a8a07eab1a5dc0e9c30fd8ded
Scott McNeil <scott@scottmcneil.ca>
2016-09-29 09:34:50 -0400

    Added block functionality to dgpcmc

--- R/monte_carlo.R
+++ R/monte_carlo.R
@@ -1,14 +1,13 @@
 #' Run one Monte Carlo repetition
 #'
-#' @param rep Integer indicating the current Monte Carlo repetition
+#' @param rep Integer indicating number of repetitions
 #' @param stat_func Function that takes a dataframe from rand_func and returns a vector of statistics
 #' @param stat_args List of arguments to be passed to stat_func
 #' @param rand_func Function that generates a dataframe of random data given rand_args
 #' @param rand_args List of arguments to be passed to stat_func
-#' @param progbar Progress bar object to be incremented
 #' @return Vector of numerical statistics
 #'
-monte_carlo_stat <- function(rep, stat_func, stat_args, rand_func, rand_args, progbar){
+monte_carlo_stat <- function(rep, stat_func, stat_args, rand_func, rand_args){
 
   #Generate set of random data
   data <- do.call(what = rand_func, args = rand_args)
@@ -18,12 +17,34 @@ monte_carlo_stat <- function(rep, stat_func, stat_args, rand_func, rand_args, pr
   #Run statistical function
   stat_vec <- do.call(what = stat_func, args = stat_args)
 
-  #Increment progbar if not NULL
-  if(class(progbar) == 'txtProgressBar'){
-    setTxtProgressBar(pb = progbar, value = rep)
+  return(stat_vec)
+
+}
+
+#' Run block of Monte Carlo repetitions
+#'
+#' @param block Integer indicating number of repetitions for block
+#' @param cumulative Integer indicating total repetitions completed including this block
+#' @param stat_func Function that takes a dataframe from rand_func and returns a vector of statistics
+#' @param stat_args List of arguments to be passed to stat_func
+#' @param rand_func Function that generates a dataframe of random data given rand_args
+#' @param rand_args List of arguments to be passed to stat_func
+#' @param pb Progress bar object to be incremented
+#' @param cores Integer indicating number of cores to use
+#' @return Vector of numerical statistics
+#'
+monte_carlo_block <- function(block, cumulative, stat_func, stat_args, rand_func, rand_args, pb, cores){
+
+  stat_mat <- parallel::mcmapply(FUN = monte_carlo_stat, rep = 1:block, mc.cores = cores,
+                                 MoreArgs = list(stat_func = stat_func, stat_args = stat_args,
+                                                 rand_func = rand_func, rand_args = rand_args))
+
+  #Increment pb if not NULL
+  if(class(pb) == 'txtProgressBar'){
+    setTxtProgressBar(pb = pb, value = cumulative)
   }
 
-  return(stat_vec)
+  return(stat_mat)
 
 }
 
@@ -35,13 +56,37 @@ monte_carlo_stat <- function(rep, stat_func, stat_args, rand_func, rand_args, pr
 #' @param rand_func Function that generates a dataframe of random data given rand_args
 #' @param rand_args List of arguments to be passed to stat_func
 #' @param progbar Progress bar object to be incremented
+#' @param cores Integer indicating number of cores to use
 #' @return Vector or matrix of numerical statistics
 #'
-monte_carlo_recur <- function(reps, stat_func, stat_args, rand_func, rand_args, progbar){
+monte_carlo_recur <- function(reps, stat_func, stat_args, rand_func, rand_args, progbar, cores){
 
-  #Run monte_carlo_stat reps times
-  stat_mat <- sapply(1:reps, monte_carlo_stat, stat_func = stat_func, stat_args = stat_args,
-                     rand_func = rand_func, rand_args = rand_args, progbar = progbar)
+  #Create pb object if TRUE
+  if(progbar == TRUE){
+    pb <- txtProgressBar(min = 0, max = reps, style = 3)
+  } else {
+    pb <- NULL
+  }
+
+  if(class(pb) == 'txtProgressBar'){
+
+    #If using progress bar, chunk up repetitions by core
+    minus_remain <- rep(cores, reps %/% cores)
+    blocks <- if(reps %% cores == 0) minus_remain else c(minus_remain, reps %% cores)
+    cumulative <- cumsum(blocks)
+
+    #Then run monte_carlo_block
+    stat_mat <- mapply(monte_carlo_block, block = blocks, cumulative = cumulative, MoreArgs = list(stat_func = stat_func, stat_args = stat_args,
+                                                                                                   rand_func = rand_func, rand_args = rand_args,
+                                                                                                   pb = pb))
+  } else{
+
+    #Otherwise run replications based on total cores selected
+    stat_mat <- parallel::mcmapply(FUN = monte_carlo_stat, rep = 1:block, mc.cores = cores,
+                                   MoreArgs = list(stat_func = stat_func, stat_args = stat_args,
+                                                   rand_func = rand_func, rand_args = rand_args))
+
+  }
 
   return(stat_mat)
 
@@ -54,16 +99,12 @@ monte_carlo_recur <- function(reps, stat_func, stat_args, rand_func, rand_args, 
 #' @param stat_args List of arguments to be passed to stat_func
 #' @param rand_func Function that generates a dataframe of random data given rand_args
 #' @param rand_args List of arguments to be passed to stat_func
-#' @param progbar Progress bar object to be incremented
+#' @param progbar Boolean for whether progress bar should be used
+#' @param cores Integer indicating number of cores to use
 #' @return Vector or matrix of numerical statistics
 #' @export
 #'
-dgpmc <- function(reps, stat_func, stat_args, rand_func, rand_args, names = NULL, progbar = FALSE){
-
-  #Create progbar object if TRUE
-  if(progbar){
-    progbar <- txtProgressBar(min = 0, max = reps, style = 3)
-  }
+dgpmc <- function(reps, stat_func, stat_args, rand_func, rand_args, names = NULL, progbar = FALSE, cores = 1){
 
   #Generate stat_mat from monte_carlo_recur
   stat_mat <- monte_carlo_recur(reps = reps, stat_func = stat_func, stat_args = stat_args,
--- tests/testthat/testinferencemc.R
+++ tests/testthat/testinferencemc.R
@@ -1,147 +1,202 @@
 library(testthat)
 library(dgpmc)
 
+#Create dummy stat function
+stat_func <- function(data, formula, x_interest, H0){
+
+  model <- lm(data = data, formula = formula)
+
+  x_coef <- coef(model)[[x_interest]]
+
+  x_se <- sqrt(diag(vcov(model)))[[x_interest]]
+
+  2*pt(abs(x_coef - H0)/x_se, df = df.residual(model), lower.tail = FALSE)
+
+}
+
+#Create dummy rand function
+rand_func <- function(n, coef){
+
+  x <- rnorm(n)
+  y <- rnorm(n) + coef*x
+
+  data.frame(y, x)
+
+}
+
 test_that('monte_carlo_stat works properly',{
 
-  stat_func <- function(data, formula, x_interest, H0){
-
-    model <- lm(data = data, formula = formula)
-
-    x_coef <- coef(model)[[x_interest]]
-
-    x_se <- sqrt(diag(vcov(model)))[[x_interest]]
-
-    2*pt(abs(x_coef - H0)/x_se, df = df.residual(model), lower.tail = FALSE)
-
-  }
-
-  rand_func <- function(n, coef, seed){
-    set.seed(seed)
-    x <- rnorm(n)
-    y <- rnorm(n) + coef*x
-    data.frame(y, x)
-  }
-
-  stat_args <- list(formula = y ~ x, x_interest = 'x', H0 = c(1, 1.2))
-  rand_args <- list(n = 10, coef = 1, seed = 42)
-  reps <- 10
-  rep <- 5
-
-  progbar <- txtProgressBar(min = 0, max = reps, style = 3)
-
-  test_p <- monte_carlo_stat(rep, stat_func = stat_func, stat_args = stat_args, rand_func = rand_func, rand_args = rand_args, progbar = progbar)
-
-  expect_equal(test_p, c(0.2857, 0.1833), tolerance = 0.0001)
-  expect_equal(getTxtProgressBar(progbar), 5)
-
+  #Set out stat and rand arguments for single returned stat value
   stat_args <- list(formula = y ~ x, x_interest = 'x', H0 = 1)
-  rand_args <- list(n = 10, coef = 1, seed = 42)
+  rand_args <- list(n = 10, coef = 1)
 
-  progbar <- txtProgressBar(min = 0, max = reps, style = 3)
+  #Create test p-value
+  set.seed(42)
+  test_p <- monte_carlo_stat(stat_func = stat_func, stat_args = stat_args, rand_func = rand_func, rand_args = rand_args)
 
-  test_p <- monte_carlo_stat(rep, stat_func = stat_func, stat_args = stat_args, rand_func = rand_func, rand_args = rand_args, progbar = progbar)
+  #Check if equal
   expect_equal(test_p, 0.2857, tolerance = 0.0001)
+
+  #Set out stat and rand arguments for multiple returned stat values
+  stat_args <- list(formula = y ~ x, x_interest = 'x', H0 = c(1, 1.2))
+  rand_args <- list(n = 10, coef = 1)
+
+  #Create test p-value
+  set.seed(42)
+  test_p <- monte_carlo_stat(stat_func = stat_func, stat_args = stat_args, rand_func = rand_func, rand_args = rand_args)
+
+  #Check if equal
+  expect_equal(test_p, c(0.2857, 0.1833), tolerance = 0.0001)
 
 })
 
-test_that('monte_carlo_recur works properly',{
+test_that('monte_carlo_block works properly',{
 
-  stat_func <- function(data, formula, x_interest, H0){
-
-    model <- lm(data = data, formula = formula)
-
-    x_coef <- coef(model)[[x_interest]]
-
-    x_se <- sqrt(diag(vcov(model)))[[x_interest]]
-
-    2*pt(abs(x_coef - H0)/x_se, df = df.residual(model), lower.tail = FALSE)
-
-  }
-
-  rand_func <- function(n, coef){
-    x <- rnorm(n)
-    y <- rnorm(n) + coef*x
-    data.frame(y, x)
-  }
-
+  #Set out arguments for single returned statistic
   stat_args <- list(formula = y ~ x, x_interest = 'x', H0 = 1)
-  rand_args <- list(n = 10, coef = 1)
-  reps <- 3
+  rand_args <- list(n = 10, coef = 1, seed = 42)
 
-  progbar <- txtProgressBar(min = 0, max = reps, style = 3)
+  cores <- 1
+  block <- 1
+  cumulative <- 3
+
+  #Create progbar object
+  pb <- txtProgressBar(min = 0, max = 5, style = 3)
 
   single_p <- c(0.2857, 0.8692, 0.5286)
 
-  set.seed(42)
-  single_test_p <- monte_carlo_recur(reps, stat_func = stat_func, stat_args = stat_args, rand_func = rand_func, rand_args = rand_args, progbar = progbar)
+  #Set seed and generate
+  set.seed(42, "L'Ecuyer")
+  single_test_p <- monte_carlo_block(block = block, cumulative = cumulative, stat_func = stat_func, stat_args = stat_args,
+                                     rand_func = rand_func, rand_args = rand_args, pb = pb, cores = cores)
 
   expect_equal(single_test_p, single_p, tolerance = 0.001)
   expect_equal(getTxtProgressBar(progbar), 3)
 
-
-  stat_args <- list(formula = y ~ x, x_interest = 'x', H0 = c(1, 1.2))
-  rand_args <- list(n = 10, coef = 1)
-  reps <- 3
-
-  progbar <- txtProgressBar(min = 0, max = reps, style = 3)
-
-  double_p <- matrix(c(0.2857, 0.1833, 0.8692, 0.4705, 0.5286, 0.8119), nrow = 2, ncol = 3)
-
-  set.seed(42)
-  double_test_p <- monte_carlo_recur(reps, stat_func = stat_func, stat_args = stat_args, rand_func = rand_func, rand_args = rand_args, progbar = progbar)
-
-  expect_equal(double_test_p, double_p, tolerance = 0.001)
-
-})
-
-test_that('dgpmc works properly',{
-
-  stat_func <- function(data, formula, x_interest, H0){
-
-    model <- lm(data = data, formula = formula)
-
-    x_coef <- coef(model)[[x_interest]]
-
-    x_se <- sqrt(diag(vcov(model)))[[x_interest]]
-
-    2*pt(abs(x_coef - H0)/x_se, df = df.residual(model), lower.tail = FALSE)
-
-  }
-
-  rand_func <- function(n, coef){
-    x <- rnorm(n)
-    y <- rnorm(n) + coef*x
-    data.frame(y, x)
-  }
-
-  stat_args <- list(formula = y ~ x, x_interest = 'x', H0 = 1)
-  rand_args <- list(n = 10, coef = 1)
-  reps <- 3
-
-  single_p <- data.frame(First = c(0.2857, 0.8692, 0.5286))
-
-  set.seed(42)
-  single_test_p <- dgpmc(reps, stat_func = stat_func, stat_args = stat_args, rand_func = rand_func, rand_args = rand_args, names = 'First')
-
-  expect_equal(single_test_p$stat, single_p, tolerance = 0.001)
-  expect_equal(single_test_p$stat_func, stat_func)
-  expect_equal(single_test_p$stat_args, stat_args)
-  expect_equal(single_test_p$rand_func, rand_func)
-  expect_equal(single_test_p$rand_args, rand_args)
-
-  stat_args <- list(formula = y ~ x, x_interest = 'x', H0 = c(1, 1.2))
-  rand_args <- list(n = 10, coef = 1)
-  reps <- 3
-
-  double_p <- data.frame(First = c(0.2857, 0.8692, 0.5286), Second = c(0.1833, 0.4705, 0.8119))
-
-  set.seed(42)
-  double_test_p <- dgpmc(reps, stat_func = stat_func, stat_args = stat_args, rand_func = rand_func, rand_args = rand_args, names = c('First','Second'))
-
-  expect_equal(double_test_p$stat, double_p, tolerance = 0.001)
-  expect_equal(double_test_p$stat_func, stat_func)
-  expect_equal(double_test_p$stat_args, stat_args)
-  expect_equal(double_test_p$rand_func, rand_func)
-  expect_equal(double_test_p$rand_args, rand_args)
+#
+#   stat_args <- list(formula = y ~ x, x_interest = 'x', H0 = c(1, 1.2))
+#   rand_args <- list(n = 10, coef = 1)
+#   reps <- 3
+#
+#   progbar <- txtProgressBar(min = 0, max = reps, style = 3)
+#
+#   double_p <- matrix(c(0.2857, 0.1833, 0.8692, 0.4705, 0.5286, 0.8119), nrow = 2, ncol = 3)
+#
+#   set.seed(42)
+#   double_test_p <- monte_carlo_recur(reps, stat_func = stat_func, stat_args = stat_args, rand_func = rand_func, rand_args = rand_args, progbar = progbar)
+#
+#   expect_equal(double_test_p, double_p, tolerance = 0.001)
+#
+#   stat_args <- list(formula = y ~ x, x_interest = 'x', H0 = c(1, 1.2))
+#   rand_args <- list(n = 10, coef = 1)
+#   reps <- 3
+#
+#   progbar <- txtProgressBar(min = 0, max = reps, style = 3)
+#
+#   double_p <- matrix(c(0.2857, 0.1833, 0.8692, 0.4705, 0.5286, 0.8119), nrow = 2, ncol = 3)
+#
+#   set.seed(42)
+#   double_test_p <- monte_carlo_recur(reps, stat_func = stat_func, stat_args = stat_args, rand_func = rand_func, rand_args = rand_args,
+#                                      progbar = progbar, cores = 2)
+#
+#   expect_equal(double_test_p, double_p, tolerance = 0.001)
 
 })
+
+# test_that('monte_carlo_recur works properly',{
+#
+#   stat_args <- list(formula = y ~ x, x_interest = 'x', H0 = 1)
+#   rand_args <- list(n = 10, coef = 1)
+#   reps <- 3
+#
+#   progbar <- txtProgressBar(min = 0, max = reps, style = 3)
+#
+#   single_p <- c(0.2857, 0.8692, 0.5286)
+#
+#   set.seed(42)
+#   single_test_p <- monte_carlo_recur(reps, stat_func = stat_func, stat_args = stat_args, rand_func = rand_func, rand_args = rand_args, progbar = progbar)
+#
+#   expect_equal(single_test_p, single_p, tolerance = 0.001)
+#   expect_equal(getTxtProgressBar(progbar), 3)
+#
+#
+#   stat_args <- list(formula = y ~ x, x_interest = 'x', H0 = c(1, 1.2))
+#   rand_args <- list(n = 10, coef = 1)
+#   reps <- 3
+#
+#   progbar <- txtProgressBar(min = 0, max = reps, style = 3)
+#
+#   double_p <- matrix(c(0.2857, 0.1833, 0.8692, 0.4705, 0.5286, 0.8119), nrow = 2, ncol = 3)
+#
+#   set.seed(42)
+#   double_test_p <- monte_carlo_recur(reps, stat_func = stat_func, stat_args = stat_args, rand_func = rand_func, rand_args = rand_args, progbar = progbar)
+#
+#   expect_equal(double_test_p, double_p, tolerance = 0.001)
+#
+#   stat_args <- list(formula = y ~ x, x_interest = 'x', H0 = c(1, 1.2))
+#   rand_args <- list(n = 10, coef = 1)
+#   reps <- 3
+#
+#   progbar <- txtProgressBar(min = 0, max = reps, style = 3)
+#
+#   double_p <- matrix(c(0.2857, 0.1833, 0.8692, 0.4705, 0.5286, 0.8119), nrow = 2, ncol = 3)
+#
+#   set.seed(42)
+#   double_test_p <- monte_carlo_recur(reps, stat_func = stat_func, stat_args = stat_args, rand_func = rand_func, rand_args = rand_args,
+#                                      progbar = progbar, cores = 2)
+#
+#   expect_equal(double_test_p, double_p, tolerance = 0.001)
+#
+# })
+#
+# test_that('dgpmc works properly',{
+#
+#   stat_func <- function(data, formula, x_interest, H0){
+#
+#     model <- lm(data = data, formula = formula)
+#
+#     x_coef <- coef(model)[[x_interest]]
+#
+#     x_se <- sqrt(diag(vcov(model)))[[x_interest]]
+#
+#     2*pt(abs(x_coef - H0)/x_se, df = df.residual(model), lower.tail = FALSE)
+#
+#   }
+#
+#   rand_func <- function(n, coef){
+#     x <- rnorm(n)
+#     y <- rnorm(n) + coef*x
+#     data.frame(y, x)
+#   }
+#
+#   stat_args <- list(formula = y ~ x, x_interest = 'x', H0 = 1)
+#   rand_args <- list(n = 10, coef = 1)
+#   reps <- 3
+#
+#   single_p <- data.frame(First = c(0.2857, 0.8692, 0.5286))
+#
+#   set.seed(42)
+#   single_test_p <- dgpmc(reps, stat_func = stat_func, stat_args = stat_args, rand_func = rand_func, rand_args = rand_args, names = 'First')
+#
+#   expect_equal(single_test_p$stat, single_p, tolerance = 0.001)
+#   expect_equal(single_test_p$stat_func, stat_func)
+#   expect_equal(single_test_p$stat_args, stat_args)
+#   expect_equal(single_test_p$rand_func, rand_func)
+#   expect_equal(single_test_p$rand_args, rand_args)
+#
+#   stat_args <- list(formula = y ~ x, x_interest = 'x', H0 = c(1, 1.2))
+#   rand_args <- list(n = 10, coef = 1)
+#   reps <- 3
+#
+#   double_p <- data.frame(First = c(0.2857, 0.8692, 0.5286), Second = c(0.1833, 0.4705, 0.8119))
+#
+#   set.seed(42)
+#   double_test_p <- dgpmc(reps, stat_func = stat_func, stat_args = stat_args, rand_func = rand_func, rand_args = rand_args, names = c('First','Second'))
+#
+#   expect_equal(double_test_p$stat, double_p, tolerance = 0.001)
+#   expect_equal(double_test_p$stat_func, stat_func)
+#   expect_equal(double_test_p$stat_args, stat_args)
+#   expect_equal(double_test_p$rand_func, rand_func)
+#   expect_equal(double_test_p$rand_args, rand_args)
+#
+# })
